ea1c156d07bc6606101d9d6de2e3dc9105e532e0
Robert Ford <rfordprojex@gmail.com>
2025-09-12 21:45:59 +0100

    chore(local): keep local changes before pulling

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: './src/schema.ts',
-  out: './drizzle',
-  driver: 'pg',
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/schema.ts",
+  out: "./drizzle",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || ''
-  }
-} satisfies Config;
+    url: process.env.DATABASE_URL!,
+  },
+});
